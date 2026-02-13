ee8b109642f41befe19836005130f9f9704e71a1
Jason Owllight <jason.s2412@gmail.com>
2026-02-13 21:06:47 +0700

    Turning off the snacks.nvim dependencie

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -9,7 +9,7 @@ return {
 			"MunifTanjim/nui.nvim",
 			"nvim-tree/nvim-web-devicons", -- Optional, but recommended
 			"antosha417/nvim-lsp-file-operations",
-			"folke/snacks.nvim",
+			-- "folke/snacks.nvim",
 		},
 		lazy = false, -- neo-tree will lazily load itself
 		use_snacks_image = false,
